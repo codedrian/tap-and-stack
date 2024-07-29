740792c611aaafb9f72ec4d9829f6bbd4bd26f94
codedrian <adriangailee@gmail.com>
2024-07-29 14:39:43 +0800

    chore: Update docker-compose.yml and add BetRiskOption component

---
+++ tap-and-stack/resources/js/Components/BetRiskOption.vue
@@ -0,0 +1,38 @@
+<script setup>
+import {useForm} from "@inertiajs/vue3";
+
+const props = defineProps({
+    bet: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+});
+
+const form = useForm({
+    bet: null
+});
+
+function submitBet() {
+    form.bet = props.bet
+}
+</script>
+
+<template>
+    <form @submit.prevent="form.post('/create')">
+        <div class="overflow-hidden shadow-sm sm:rounded-lg border-2 border-red-600 text-center">
+            <h2>{{ props.bet }}</h2>
+            <button type="submit" class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+                    :disabled="form.processing"
+                    @click="submitBet">Bet</button>
+            <p>{{  props.description }}</p>
+        </div>
+    </form>
+</template>
+
+<style scoped>
+
+</style>
