21f95d598811f335767ff73366bae0b1d047790b
codedrian <adriangailee@gmail.com>
2024-07-31 18:06:22 +0800

    Chore: Change betRiskOption component border color

--- tap-and-stack/resources/js/Components/BetRiskOption.vue
+++ tap-and-stack/resources/js/Components/BetRiskOption.vue
@@ -23,9 +23,10 @@ function submitBet() {
 
 <template>
     <form @submit.prevent="form.post('/create')">
-        <div class="overflow-hidden shadow-sm sm:rounded-lg border-2 border-red-600 text-center">
+        <div class="overflow-hidden shadow-sm sm:rounded-lg border-2 border-blue-600 text-center">
             <h2>{{ props.bet }}</h2>
-            <button type="submit" class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+            <button type="submit"
+                    class="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded my-4"
                     :disabled="form.processing"
                     @click="submitBet">Bet</button>
             <p>{{  props.description }}</p>
