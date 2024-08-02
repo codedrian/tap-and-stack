0e03ac5849582c8a87facb567c6c0decd33f7663
Adrian Gaile Singh <adriangaile.singh@gmail.com>
2024-08-02 12:27:45 +0800

    Chore: Change the available_balance validation rule to Integer
    Signed-off-by: Adrian Gaile Singh <adriangaile.singh@gmail.com>

--- tap-and-stack/app/Http/Requests/BetRequest.php
+++ tap-and-stack/app/Http/Requests/BetRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class BetRequest extends FormRequest
@@ -17,13 +18,13 @@ class BetRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
         return [
             'bet' => ['required', 'String'],
-            'available_balance' => ['required', 'String'],
+            'available_balance' => ['required', 'Integer'],
             'user_id' => ['required', 'Integer']
         ];
     }
