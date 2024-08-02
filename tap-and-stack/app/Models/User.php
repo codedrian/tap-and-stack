f1417ca4420c31c4b7291fde7ccef5def0e75aae
Adrian Gaile Singh <adriangaile.singh@gmail.com>
2024-08-02 11:51:10 +0800

    Chore: Add hasMany relationship to Bet model

--- tap-and-stack/app/Models/User.php
+++ tap-and-stack/app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -49,4 +50,10 @@ class User extends Authenticatable
     protected $attributes = [
         'balance' => 500.00,
     ];
+
+    /*Relationship*/
+    public function bets() :HasMany
+    {
+        return $this->hasMany(Bet::class);
+    }
 }
