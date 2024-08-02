a25a072e981a246b874aa8e46795416feaa74b30
Adrian Gaile Singh <adriangaile.singh@gmail.com>
2024-08-02 12:26:09 +0800

    Chore: Add logic for bet winnings
    Signed-off-by: Adrian Gaile Singh <adriangaile.singh@gmail.com>

--- tap-and-stack/app/Http/Controllers/BetController.php
+++ tap-and-stack/app/Http/Controllers/BetController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\BetRequest;
 use App\Models\Bet;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Event;
+
 
 class BetController extends Controller
 {
@@ -19,12 +22,42 @@ class BetController extends Controller
     /**
      * Show the form for creating a new resource/bet.
      */
-    public function create(BetRequest $request)
+    public function create(BetRequest $request, Event $event)
     {
+        /*TODO:
+            1. Randomize the result */
         $validated = $request->validated();
+        $available_balance = $validated['available_balance'];
+        $new_balance = 0;
+        $net_winnings = 0;
+
         if ($validated['bet'] == 'Low Risk') {
-            dd('Low risk it is');
+            $net_winnings = rand(-25, 100);
+            $new_balance = $available_balance + $net_winnings;
+        } elseif ($validated['bet'] == 'Moderate Risk') {
+            $net_winnings = rand(-100, 1000);
+            $new_balance = $available_balance + $net_winnings;
+        } elseif ($validated['bet'] == 'High Risk') {
+            $net_winnings = rand(-500, 2500);
+            $new_balance = $available_balance + $net_winnings;
+        } elseif ($validated['bet'] == 'Severe Risk') {
+            $net_winnings = rand(-3000, 5000);
+            $new_balance = $available_balance + $net_winnings;
         }
+        /*Note: Save into db*/
+        $bet = Bet::create([
+            'user_id' => $validated['user_id'],
+            'bet' => $validated['bet'],
+            'net_winnings' => $net_winnings,
+            'available_balance' => $validated['available_balance'],
+            'new_balance' => $new_balance
+        ]);
+        $user = User::find($validated['user_id']);
+        $result = $user->bets;
+
+        return response()->json([
+            'result' => $result
+        ], 201);
     }
 
     /**
